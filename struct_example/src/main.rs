aa0de7ac739563e00718119aa669bd8aebd00872
quangnv-java <quangnv@ominext.com>
2020-02-19 08:28:06 +0000

    update: struct continue

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -1,9 +1,24 @@
+#[derive(Debug)]
 struct User {
     username: String,
     email: String,
     sign_in_account: u64,
     active: bool
 }
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    // add code here
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 
 fn main() {
     let mut user = User {
@@ -27,6 +42,39 @@ fn main() {
     };
     println!("{}", user2.username);
 
+    // function with 2 variable
+    println!("the area = {}", areaOrigin(30, 50));
+
+
+    // refactoring variable by tuple 
+    let rect1 = (30, 50);
+    println!("the area = {}", area(rect1));
+
+    // refactoring variable by struct 
+    let rect2 = Rectangle {
+        width: 30, 
+        height: 50
+    };
+    println!("the area = {}", areaStruct(rect2));
+
+
+    let rect3 = Rectangle { width: 30, height: 50 };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect3.area()
+    );
+}
+
+fn areaStruct(dimensions: Rectangle) -> u32{
+    dimensions.width * dimensions.height
+}
+
+fn areaOrigin(width: u32, height: u32) ->u32 {
+    width * height
+}
+
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
 
 
