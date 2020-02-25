af2da468417ff247c895c41f872ce4d8a265103d
OnlyKasa <quangnguyen6295@gmail.com>
2020-02-25 20:27:01 +0700

    merge

--- structure_project/src/main.rs
+++ structure_project/src/main.rs
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-mod lib;
-
-
-fn main() {
-    println!("Hello, world!");
-
-    
-=======
 
 fn main() {
     println!("Hello, world!");
     lib::create_lib();
->>>>>>> 599a01fdcc09d9bcad1d3b6f17e1caa991d94aa2
 }
