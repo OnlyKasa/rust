b60e879ea27f89a318cc0aee8c9e91831c4f78f7
OnlyKasa <quangnguyen6295@gmail.com>
2020-03-02 21:18:13 +0700

    add common collections

---
+++ common_collections/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    let v1: Vec<i32> = Vec::new();
+
+    let v2 = vec![1,2,3];
+
+    println!("vertor one {:?}", v1);
+    println!("vertor two {:?}", v2);
+}
