695380ca256afade31b1f09497b57fcedecca4bc
OnlyKasa <quangnguyen6295@gmail.com>
2020-03-03 22:14:50 +0700

    update code

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -4,6 +4,20 @@ fn main() {
 
     let v2 = vec![1,2,3];
 
-    println!("vertor one {:?}", v1);
-    println!("vertor two {:?}", v2);
+    println!("vector one {:#?}", v1);
+    println!("vector two {:#?}", v2);
+
+
+    // update vector
+    let mut v = Vec::new();
+    v.push("1");
+    v.push("2");
+   // v.push("3"); /// error. the value no same type with others value 
+
+    let first = &v[0];
+
+    // println!("first {:?}", first);
+
+    v.push("3"); // error when update vector when holding a reference to an item
+    println!("first {:?}", first);
 }
